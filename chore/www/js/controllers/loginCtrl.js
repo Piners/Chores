e49265bb194603e8b2a4a7492fb6fe7b65037b3c
alljsh <alljsh@yahoo.com>
2016-10-26 09:54:13 -0600

    login oauth

--- chore/www/js/controllers/loginCtrl.js
+++ chore/www/js/controllers/loginCtrl.js
@@ -1,8 +1,11 @@
-angular.module('chore').controller("loginCtrl", function($scope){
+angular.module('chore').controller("loginCtrl", function($scope, $auth){
+
+  $scope.authenticate = function(provider) {
+  $auth.authenticate(provider);
   // $scope.test = "Hello from login controller"
   $scope.revealer = function(){
   $scope.hide = !$scope.hide;
-
+  }
  }
 
 })
