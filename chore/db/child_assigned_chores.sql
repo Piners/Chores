34b31eaa3481d94ed07124fdf374364fa99fa3ac
Amanda <bell-aman@hotmail.com>
2016-10-28 11:32:05 -0600

    db updates

---
+++ chore/db/child_assigned_chores.sql
@@ -0,0 +1,5 @@
+SELECT user_first_name,user_last_name,user_image,user_points_total,chore_name,
+chore_description,chore_value,chore_daily,chore_weekly,
+chore_monthly,chore_status FROM users
+JOIN assigned_chores ON users.user_id_pk = assigned_chores.user_id_fk
+WHERE user_id_pk = 8 AND chore_status = false;
