8e8b91068345933bdd596438150a4583a7768fc3
Andrew <andrew.mecham@gmail.com>
2016-11-01 10:01:31 -0600

    fixed cors issue

--- chore/www/js/service/userService.js
+++ chore/www/js/service/userService.js
@@ -24,27 +24,11 @@ this.getbanner = function(userId){
 
 
 this.getWeather = function(){
-  // var deffered = $q.defer();
-  // console.log($http.defaults);
-//
 return $http({
     method: "GET",
     url:'http://api.openweathermap.org/data/2.5/weather?q=zip=84010&units=imperial&appid=c10ef99c5afdee3fdfba78e8c981a9b6'
-    // 'http://api.openweathermap.org/data/2.5/weather?q=zip=84010&units=imperial&appid=81f9c03043ce1585b1ab81cd8a1f4963'
   })
-// httpRequest = new XMLHttpRequest();
-// httpRequest.onreadystatechange = function(){
-//
-//   if (httpRequest.readyState === XMLHttpRequest.DONE) {
-//       // everything is good, the response is received
-//   } else {
-//       // still not ready
-//   }
-//
-// }
-// httpRequest.open('GET',
-// 'http://api.openweathermap.org/data/2.5/weather?q=zip=84010&units=imperial&appid=c10ef99c5afdee3fdfba78e8c981a9b6')
-// httpRequest.send(null);
+
 };
 
 
