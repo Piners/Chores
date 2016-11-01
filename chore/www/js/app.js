e9a6a78b1887531c6c961f4a6fa357b0570db0e0
Amanda <bell-aman@hotmail.com>
2016-11-01 10:13:24 -0600

    start camera modal

--- chore/www/js/app.js
+++ chore/www/js/app.js
@@ -3,14 +3,11 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-<<<<<<< HEAD
-angular.module('chore', ['ionic', 'ngCordova']) 
-.config(function($stateProvider){
-=======
+
 angular.module('chore', ['ionic', 'satellizer'])
 .config(function($stateProvider, $urlRouterProvider, $authProvider){
-  $urlRouterProvider.otherwise("/home");
->>>>>>> master
+  $urlRouterProvider.otherwise("/tracker/");
+
   $stateProvider
   .state('addChild',{
     url:"/addChild",
