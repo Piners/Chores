9543b5d7a7ed6678df1c781f1263caf0e9872449
Andrew <andrew.mecham@gmail.com>
2016-11-04 15:22:02 -0600

    added delete chore endpoint

--- chore/server.js
+++ chore/server.js
@@ -278,6 +278,10 @@ monthlyReset.start();
     //delete a reward
     app.delete('/reward', reward.removeReward);
 
+    // delete an assigned chores
+    //  ** use the assigned_chore_pk as the param **
+    app.delete('/chore/:id',ensureAuthenticated, chores.deletechore);
+
     // keep this at the end of file
     app.listen(config.port, function() {
       console.log('listening on port', config.port);
