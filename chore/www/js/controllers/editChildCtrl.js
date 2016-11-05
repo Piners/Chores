c44d5d9ea76f26ff8fbba789f535db965794631a
phogge <phogge@gmail.com>
2016-11-04 18:33:53 -0600

    edit child

--- chore/www/js/controllers/editChildCtrl.js
+++ chore/www/js/controllers/editChildCtrl.js
@@ -1,29 +1,44 @@
-angular.module('chore').controller("editChildCtrl", function($scope,userService,$state){
+angular.module('chore').controller("editChildCtrl", function($scope,userService,$state,$auth){
   //backbutton
   $scope.goback = function(){
     window.history.go(-1);
   };
 
-  var user = userService.getUserInfo.sub;
-  console.log(user);
-  $scope.changeImage = function(image){
-    image.user_household = user.user_household;
-    console.log(image);
-    userService.addImage(image)
+  var userInfo = $auth.getPayload().sub;
+  console.log(userInfo);
+  $scope.username = userInfo.user_first_name;
+
+  $scope.childSubmitImage = function(image){
+    userService.updateImage(userInfo.user_id_pk,image)
     .then(function(res){
-      $state.go('editChild');
+      document.getElementById("child-setting-image").value = '';
+    });
+  };   id="child-setting-image"
+
+  $scope.childSubmitName = function(name){
+    userService.updateName(userInfo.user_id_pk,name)
+    .then(function(res){
+      document.getElementById("child-setting-name").value = '';
     });
   };
 
-  var user = userService.getUserInfo.sub;
-  console.log(user);
-  $scope.changeName = function(name){
-    name.user_household = user.user_household;
-    console.log(image);
-    userService.addName(name)
+  $scope.childSubmitEmail = function(email){
+    userService.updateEmail(userInfo.user_id_pk,email)
     .then(function(res){
-      $state.go('editChild');
+      document.getElementById("child-setting-email").value = '';
     });
   };
+
+  $scope.childSubmitPassword = function(password){
+    console.log(userInfo.user_id_pk);
+    userService.updatePassword(userInfo.user_id_pk,user_password)
+    .then(function(res){
+          document.getElementById("child-setting-password").value = '';
+    });
+  };
+
+
+
+
 
 });
