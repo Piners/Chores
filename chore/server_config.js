acf4580308f12f92f2b8921987ef7b4be812c38c
root <alljsh@yahoo.com>
2016-11-11 02:46:35 +0000

    update

---
+++ chore/server_config.js
@@ -0,0 +1,6 @@
+{
+"port": 8100,
+"sessionSecret": "removediamondalligatorfox0987654323456789876543456787654",
+"connectionString":"postgres://ebodbpbj:-Mi4bWE25TMROE0A_qqdxwg8HgHfL0bj@elmer.db.elephantsql.com:5432/ebodbpbj",
+"TOKEN_SECRET": "trampledByKittens"
+}
